2522bee7af9f2d27cdf8328af1a01787239099cd
soojincode <soojincode@gmail.com>
2023-06-02 15:17:05 +0900

    컨트롤러 오류 수정 중

--- passioncode/src/main/java/com/passioncode/procurementsystem/controller/PS2Controller.java
+++ passioncode/src/main/java/com/passioncode/procurementsystem/controller/PS2Controller.java
@@ -2,20 +2,32 @@ package com.passioncode.procurementsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.passioncode.procurementsystem.dto.PurchaseOrderDTO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("procurement2")
+@Log4j2
 public class PS2Controller {
 	
-	@GetMapping("procurement2/purchaseOrder")
+	@GetMapping("/purchaseOrder")
 	public String PS2Test(PurchaseOrderDTO purchaseOrderDTO) {
 		log.info(">>>>>>>"+purchaseOrderDTO);
+		return "/procurement2/purchaseOrder";
 		
-		return "procurement2/purchaseOrder";
+		
+	}
+	
+	@GetMapping("/progressCheck")
+	public String PS2Test2(PurchaseOrderDTO purchaseOrderDTO) {
+		log.info(">>>>>>>"+purchaseOrderDTO);
+		return "/procurement2/progressCheck";
+		
 		
 	}
 
