6a975624e36f32aa7262b68403f1b6f464269176
soojincode <soojincode@gmail.com>
2023-06-04 23:43:58 +0900

    발주서 발행 상세 화면 추가
    detailPurchaseOrder는 발주서 발행전 상세 화면

--- passioncode/src/main/java/com/passioncode/procurementsystem/controller/PS2Controller.java
+++ passioncode/src/main/java/com/passioncode/procurementsystem/controller/PS2Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.passioncode.procurementsystem.dto.DetailPurchaseOrderDTO;
 import com.passioncode.procurementsystem.dto.PurchaseOrderDTO;
+import com.passioncode.procurementsystem.service.DetailPurchaseOrderService;
 
 import ch.qos.logback.core.model.Model;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,12 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class PS2Controller {
 	
+	
 	@GetMapping("/purchaseOrder")
 	public String PS2Test(Model model,PurchaseOrderDTO purchaseOrderDTO, DetailPurchaseOrderDTO detailDTO) {
 		log.info(">>>>>>>"+purchaseOrderDTO);
 		log.info("세부사항 >>>>>>>"+detailDTO);
+		
 		return "/procurement2/purchaseOrder";
 		
 		
@@ -30,6 +33,14 @@ public class PS2Controller {
 	public String PS2Test2(PurchaseOrderDTO purchaseOrderDTO) {
 		log.info(">>>>>>>"+purchaseOrderDTO);
 		return "/procurement2/progressCheck";
+		
+		
+	}
+	
+	@GetMapping("/detailPurchaseOrder")
+	public void PS2Test22(PurchaseOrderDTO purchaseOrderDTO) {
+		log.info(">>>>>>>"+purchaseOrderDTO);
+		
 		
 		
 	}
--- passioncode/src/main/java/com/passioncode/procurementsystem/service/DetailPurchaseOrderService.java
+++ passioncode/src/main/java/com/passioncode/procurementsystem/service/DetailPurchaseOrderService.java
@@ -2,5 +2,9 @@ package com.passioncode.procurementsystem.service;
 import com.passioncode.procurementsystem.dto.DetailPurchaseOrderDTO;
 
 public interface DetailPurchaseOrderService {
+	
+	//PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+	//GuestbookDTO read(Long gno);
+	DetailPurchaseOrderDTO read(Integer no);
 
 }
