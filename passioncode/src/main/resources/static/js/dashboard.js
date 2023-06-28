5da1340c3f39d67d1c0d4b8802e9d860cd52c87b
limna15 <skskdudu15@gmail.com>
2023-06-29 00:09:54 +0900

    재고금액 현황관리 그래프 4개 띄우기

--- passioncode/src/main/resources/static/js/dashboard.js
+++ passioncode/src/main/resources/static/js/dashboard.js
@@ -1,100 +1,8 @@
 (function($) {
   'use strict';
+  
+  //테이블에 관련된 스크립트
   $(function() {
-
-    if ($('#chart').length) {
-      var cashDepositsCanvas = $("#chart").get(0).getContext("2d");
-      var data = {
-        labels: ["2023-06-09", "2023-06-10", "2023-06-11", "2023-06-12", "2023-06-13", "2023-06-14"],
-        datasets: [
-          {
-            label: 'Sales',
-            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
-            borderColor: [
-              '#4d83ff'
-            ],
-            borderWidth: 2,
-            fill: false,
-            pointBackgroundColor: "#fff"
-          }
-        ]
-      };
-      var options = {
-        scales: {
-          yAxes: [{
-            display: true,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-              zeroLineColor: "#e9e9e9",
-            },
-            ticks: {
-              min: 0,
-              max: 100,
-              stepSize: 20,
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }],
-          xAxes: [{
-            display: true,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-            },
-            ticks : {
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }]
-        },
-        legend: {
-          display: false
-        },
-        legendCallback: function(chart) {
-          var text = [];
-          text.push('<ul class="dashboard-chart-legend">');
-          for(var i=0; i < chart.data.datasets.length; i++) {
-            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
-            if (chart.data.datasets[i].label) {
-              text.push(chart.data.datasets[i].label);
-            }
-          }
-          text.push('</ul>');
-          return text.join("");
-        },
-        elements: {
-          point: {
-            radius: 3
-          },
-          line :{
-            tension: 0
-          }
-        },
-        stepsize: 1,
-        layout : {
-          padding : {
-            top: 0,
-            bottom : -10,
-            left : -10,
-            right: 0
-          }
-        }
-      };
-      var cashDeposits = new Chart(cashDepositsCanvas, {
-        type: 'line',
-        data: data,
-        options: options
-      });
-      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
-    }
-
     if ($('#total-sales-chart').length) {
       var totalSalesChartCanvas = $("#total-sales-chart").get(0).getContext("2d");
 
@@ -222,6 +130,393 @@
       },
       searching: false, paging: false, info: false
     });
-
+  });
+  
+  
+  //첫번째 차트
+  $(function() {
+    if ($('#chart').length) {
+      var cashDepositsCanvas = $("#chart").get(0).getContext("2d");
+      var data = {
+        labels: ["2023-06-09", "2023-06-10", "2023-06-11", "2023-06-12", "2023-06-13", "2023-06-14"],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
+            borderColor: [
+              '#4d83ff'
+            ],
+            borderWidth: 2,
+            fill: false,
+            pointBackgroundColor: "#fff"
+          }
+        ]
+      };
+      var options = {
+        scales: {
+          yAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+              zeroLineColor: "#e9e9e9",
+            },
+            ticks: {
+              min: 0,
+              max: 100,
+              stepSize: 20,
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }],
+          xAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+            },
+            ticks : {
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        legendCallback: function(chart) {
+          var text = [];
+          text.push('<ul class="dashboard-chart-legend">');
+          for(var i=0; i < chart.data.datasets.length; i++) {
+            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
+            if (chart.data.datasets[i].label) {
+              text.push(chart.data.datasets[i].label);
+            }
+          }
+          text.push('</ul>');
+          return text.join("");
+        },
+        elements: {
+          point: {
+            radius: 3
+          },
+          line :{
+            tension: 0
+          }
+        },
+        stepsize: 1,
+        layout : {
+          padding : {
+            top: 0,
+            bottom : -10,
+            left : -10,
+            right: 0
+          }
+        }
+      };
+      var cashDeposits = new Chart(cashDepositsCanvas, {
+        type: 'line',
+        data: data,
+        options: options
+      });
+      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
+    }
+  });
+  
+  
+  //두번째 차트
+  $(function() {
+    if ($('#chart2').length) {
+      var cashDepositsCanvas = $("#chart2").get(0).getContext("2d");
+      var data = {
+        labels: ["2023-06-09", "2023-06-10", "2023-06-11", "2023-06-12", "2023-06-13", "2023-06-14"],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
+            borderColor: [
+              '#4d83ff'
+            ],
+            borderWidth: 2,
+            fill: false,
+            pointBackgroundColor: "#fff"
+          }
+        ]
+      };
+      var options = {
+        scales: {
+          yAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+              zeroLineColor: "#e9e9e9",
+            },
+            ticks: {
+              min: 0,
+              max: 100,
+              stepSize: 20,
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }],
+          xAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+            },
+            ticks : {
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        legendCallback: function(chart) {
+          var text = [];
+          text.push('<ul class="dashboard-chart-legend">');
+          for(var i=0; i < chart.data.datasets.length; i++) {
+            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
+            if (chart.data.datasets[i].label) {
+              text.push(chart.data.datasets[i].label);
+            }
+          }
+          text.push('</ul>');
+          return text.join("");
+        },
+        elements: {
+          point: {
+            radius: 3
+          },
+          line :{
+            tension: 0
+          }
+        },
+        stepsize: 1,
+        layout : {
+          padding : {
+            top: 0,
+            bottom : -10,
+            left : -10,
+            right: 0
+          }
+        }
+      };
+      var cashDeposits = new Chart(cashDepositsCanvas, {
+        type: 'line',
+        data: data,
+        options: options
+      });
+      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
+    }
+  });
+  
+  
+  //세번째 차트
+  $(function() {
+    if ($('#chart3').length) {
+      var cashDepositsCanvas = $("#chart3").get(0).getContext("2d");
+      var data = {
+        labels: ["2023-06-09", "2023-06-10", "2023-06-11", "2023-06-12", "2023-06-13", "2023-06-14"],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
+            borderColor: [
+              '#4d83ff'
+            ],
+            borderWidth: 2,
+            fill: false,
+            pointBackgroundColor: "#fff"
+          }
+        ]
+      };
+      var options = {
+        scales: {
+          yAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+              zeroLineColor: "#e9e9e9",
+            },
+            ticks: {
+              min: 0,
+              max: 100,
+              stepSize: 20,
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }],
+          xAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+            },
+            ticks : {
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        legendCallback: function(chart) {
+          var text = [];
+          text.push('<ul class="dashboard-chart-legend">');
+          for(var i=0; i < chart.data.datasets.length; i++) {
+            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
+            if (chart.data.datasets[i].label) {
+              text.push(chart.data.datasets[i].label);
+            }
+          }
+          text.push('</ul>');
+          return text.join("");
+        },
+        elements: {
+          point: {
+            radius: 3
+          },
+          line :{
+            tension: 0
+          }
+        },
+        stepsize: 1,
+        layout : {
+          padding : {
+            top: 0,
+            bottom : -10,
+            left : -10,
+            right: 0
+          }
+        }
+      };
+      var cashDeposits = new Chart(cashDepositsCanvas, {
+        type: 'line',
+        data: data,
+        options: options
+      });
+      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
+    }
+  });
+  
+  
+  //네번째 차트
+  $(function() {
+    if ($('#chart4').length) {
+      var cashDepositsCanvas = $("#chart4").get(0).getContext("2d");
+      var data = {
+        labels: ["2023-06-09", "2023-06-10", "2023-06-11", "2023-06-12", "2023-06-13", "2023-06-14"],
+        datasets: [
+          {
+            label: 'Sales',
+            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
+            borderColor: [
+              '#4d83ff'
+            ],
+            borderWidth: 2,
+            fill: false,
+            pointBackgroundColor: "#fff"
+          }
+        ]
+      };
+      var options = {
+        scales: {
+          yAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+              zeroLineColor: "#e9e9e9",
+            },
+            ticks: {
+              min: 0,
+              max: 100,
+              stepSize: 20,
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }],
+          xAxes: [{
+            display: true,
+            gridLines: {
+              drawBorder: false,
+              lineWidth: 1,
+              color: "#e9e9e9",
+            },
+            ticks : {
+              fontColor: "#6c7383",
+              fontSize: 16,
+              fontStyle: 300,
+              padding: 15
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        legendCallback: function(chart) {
+          var text = [];
+          text.push('<ul class="dashboard-chart-legend">');
+          for(var i=0; i < chart.data.datasets.length; i++) {
+            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
+            if (chart.data.datasets[i].label) {
+              text.push(chart.data.datasets[i].label);
+            }
+          }
+          text.push('</ul>');
+          return text.join("");
+        },
+        elements: {
+          point: {
+            radius: 3
+          },
+          line :{
+            tension: 0
+          }
+        },
+        stepsize: 1,
+        layout : {
+          padding : {
+            top: 0,
+            bottom : -10,
+            left : -10,
+            right: 0
+          }
+        }
+      };
+      var cashDeposits = new Chart(cashDepositsCanvas, {
+        type: 'line',
+        data: data,
+        options: options
+      });
+      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
+    }
   });
 })(jQuery);
